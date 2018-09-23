71f801b4267845381302dfebe5890832417becc9
HieuQB <hieuit275@gmail.com>
2018-09-23 15:32:03 +0700

    fix bug open shift

--- config/main.js
+++ config/main.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'secret': 'yummy',
-    'dbUri': 'mongodb://admin:xxxxxxxxx@dsxxxxx.mlab.com:xxxxx/appname'
+    port: '8080',
+    host: '172.30.33.108',
+    environment: 'production'
 };
