63ca34d48f87c0c4b4df4fdc0006cc1e74d88c3a
HieuQB <hieuit275@gmail.com>
2018-09-23 23:01:33 +0700

    sua port ket noi

--- helpers/config.js
+++ helpers/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     'jwtSecret': 'yummy',
-    'dbUri': 'mongodb://admin:xxxxxxxxx@dsxxxxx.mlab.com:xxxxx/appname'
+    'dbUri': 'mongodb://admin:xxxxxxxxx@dsxxxxx.mlab.com:xxxxx/appname',
+    port: '8080',
+    host: '172.30.33.108',
+    environment: 'production'
 };
