02a22eb249fdd034c9f95ec240bb330db7075a5f
HieuQB <hieuit275@gmail.com>
2018-09-27 23:18:21 +0700

    a

--- models/UserModel.js
+++ models/UserModel.js
@@ -56,7 +56,7 @@ var UserSchema = new Schema({
 }, {
     versionKey: false
 });
-UserSchema.plugin(autoIncrement.plugin,{model: 'User', field: 'id'});
+UserSchema.plugin(autoIncrement.plugin,{model: 'User'});
 
 // Saves the user's password hashed (plain text password storage is not good)
 UserSchema.pre('save', function (next) {
