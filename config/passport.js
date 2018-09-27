4b35ac2a6a5593c28895564fe5aedcff250ad92b
HieuQB <hieuit275@gmail.com>
2018-09-27 23:30:44 +0700

    tat paspost

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,24 @@
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
-// var User = require('../models/userModel');
-// var config = require('../config/main');
+var User = require('../models/UserModel.js');
+var config = require('../config/main.js');
 
 // Setup work and export for the JWT passport strategy
 module.exports = function(passport) {
-    // var opts = {};
-    // opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-    // opts.secretOrKey = config.secret;
-    // opts.expiresIn = 604800;
-    // passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    //     User.findOne({email: jwt_payload.email}, function(err, user) {
-    //         if (err) {
-    //             return done(err, false);
-    //         }
-    //         if (user) {
-    //             done(null, user);
-    //         } else {
-    //             done(null, false);
-    //         }
-    //     });
-    // }));
+    var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+    opts.secretOrKey = config.secret;
+    opts.expiresIn = 604800;
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({email: jwt_payload.email}, function(err, user) {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                done(null, user);
+            } else {
+                done(null, false);
+            }
+        });
+    }));
 };
