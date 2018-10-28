2df8bc4e84451655f81558953fb9fb3fa7381884
HieuQB <hieuit275@gmail.com>
2018-10-28 10:00:42 +0700

    a

--- models/NotificationModel.js
+++ models/NotificationModel.js
@@ -10,7 +10,7 @@ var NotificationSchema = new Schema({
     // type: {type: Number,default:0},
     title: {type: String},
     created_date: {type: Date, default: Date.now()},
-    content: {type: Object, ref: "NotiData"},
+    content: {type: Object},
     image: {type: String, default: ""}
 }, {
     versionKey: false
